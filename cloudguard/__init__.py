f747465ac0ff00ca798abf96382ece307d9e62ec
Jimmy Thrasibule <dev@jimmy.lt>
2023-03-16 17:19:27 +0100

    session: Add the Session class
    The Session is the central object of the CloudGuard SDK. It consolidates
    everything that is required to communicate with CloudGuard.
    
    Signed-off-by: Jimmy Thrasibule <dev@jimmy.lt>

--- cloudguard/__init__.py
+++ cloudguard/__init__.py
@@ -18,7 +18,73 @@
 # You should have received a copy of the MIT License along with *cloudguard*.
 # If not, see <http://opensource.org/licenses/MIT>.
 #
+import typing as ty
+import asyncio
 import logging
+
+from cloudguard.session import Session, AsyncSession
+
+
+#: The default CloudGuard session.
+_DEFAULT_SESSION: ty.Optional[ty.Union[AsyncSession, Session]] = None
+
+
+def default_session() -> ty.Union[AsyncSession, Session]:
+    """Get the SDK's default session.
+
+
+    :returns: The session used by the SDK by default.
+    :rtype: ~typing.Union[~cloudguard.session.Session, ~cloudguard.session.AsyncSession]
+
+    """
+    if _DEFAULT_SESSION is None:
+        setup_default_session()
+    return _DEFAULT_SESSION
+
+
+def session(
+    force_async: bool = False, force_sync: bool = False, *args, **kwargs
+) -> ty.Union[AsyncSession, Session]:
+    """Create a new CloudGuard session. By default, when an asynchronous runtime
+    is detected, an asynchronous session is returned. This can be changed by the
+     ``force_async`` or ``force_sync`` parameters.
+
+     Any other parameters are provided to the session's constructor.
+
+
+    :param force_async: Force the returned session to be asynchronous.
+    :type force_async: bool
+
+    :param force_sync: Force the returned session to be synchronous.
+    :type force_sync: bool
+
+
+    :returns: A CloudGuard session.
+    :rtype: ~typing.Union[~cloudguard.session.Session, ~cloudguard.session.AsyncSession]
+
+    """
+    if force_async and force_sync:
+        raise ValueError("`force_async` and `force_sync` cannot be set both.")
+    if force_async:
+        return AsyncSession(*args, **kwargs)
+    if force_sync:
+        return Session(*args, **kwargs)
+
+    try:
+        asyncio.get_running_loop()
+    except RuntimeError:
+        return Session(*args, **kwargs)
+    else:
+        return AsyncSession(*args, **kwargs)
+
+
+def setup_default_session(*args, **kwargs) -> None:
+    """Set up the SDK's default session. All parameters are provided to the
+    session maker.
+
+    """
+    global _DEFAULT_SESSION
+    _DEFAULT_SESSION = session(*args, **kwargs)
 
 
 # Ensure that the library does not emit log messages.
---
+++ cloudguard/session.py
@@ -0,0 +1,37 @@
+# cloudguard/session.py
+# =====================
+#
+# Copying
+# -------
+#
+# Copyright (c) 2023 cloudguard authors and contributors.
+#
+# This file is part of the *cloudguard* project.
+#
+# *cloudguard* is a free software project. You can redistribute it and/or
+# modify it following the terms of the MIT License.
+#
+# This software project is distributed *as is*, WITHOUT WARRANTY OF ANY KIND;
+# including but not limited to the WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE and NONINFRINGEMENT.
+#
+# You should have received a copy of the MIT License along with *cloudguard*.
+# If not, see <http://opensource.org/licenses/MIT>.
+#
+
+
+class Session(object):
+    """The session consolidates in a single place everything required to
+    communicate with the CloudGuard API.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Constructor for :class:`cloudguard.session.Session`."""
+        pass
+
+
+class AsyncSession(Session):
+    """The asynchronous session is to be used in a concurrent runtime."""
+
+    pass
